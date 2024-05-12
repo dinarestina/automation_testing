ebcd0d61d9f6515f15d1807c5e9b2fa6788092db
DESKTOP-51NBDP8\User <dinarestina03@gmail.com>
2024-05-12 17:00:01 +0700

    pertemuan 2: add scenarion login

--- features/step_definitions/login_step.rb
+++ features/step_definitions/login_step.rb
@@ -1,5 +1,5 @@
 Given('I am on the homepage automationexercise.com') do
-    visit('https://automationexercise.com')
+    visit"https://automationexercise.com"
   end
   
   When('I click sign in on the homepage') do
@@ -7,9 +7,15 @@ Given('I am on the homepage automationexercise.com') do
   end
   
   When('I fill my credentials on the homepage') do
-    pending # Write code here that turns the phrase above into concrete actions
+    # //*[@data-qa='login-email']
+    find(:xpath, "//*[@data-qa='login-email']").set("wele.testing@gmail.com")
+    find(:xpath, "//*[@data-qa='login-password']").set("abc123")
+    find(:xpath, "//button[text()='Login']").click
+    # find(:xpath, "//*[@data-qa='login-password']//following-sibling::button").click
   end
   
   Then('I should be logged in') do
-    pending # Write code here that turns the phrase above into concrete actions
+    assert_current_path("https://automationexercise.com/", wait: 5)
+    # ada button delete account
+    find(:xpath, "//*[contains(text(), 'Delete Account')]").should be_visible
   end
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,14 +2,14 @@ Before do |scenario|
     page.windows[0].maximize
   end
   
-  After do |scenario|
-    sleep 10
-  end
+#   After do |scenario|
+#     sleep 10
+#   end
   
-  Before('@test-login-123') do
-    puts "HOOKS BEFORE DIJALANKAN!"
-  end
+#   Before('@test-login-123') do
+#     puts "HOOKS BEFORE DIJALANKAN!"
+#   end
   
-  After('@test-login') do
-    # puts "HOOKS AFTER TAGGED DIJALANKAN!"
-  end
+#   After('@test-login') do
+#     # puts "HOOKS AFTER TAGGED DIJALANKAN!"
+#   end
